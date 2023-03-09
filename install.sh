56225c34179220c2aba34567e22e9437f38e9fb2
GuYp <gu.yp@qq.com>
2023-03-09 10:54:41 +0800

    source ~/.bashrc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 make clean && make && sudo cp ./trash /usr/local/bin/
-echo "alias rm='/usr/local/bin/trash'" >> ~/.bashrc
 echo "alias rm='/usr/local/bin/trash -d'" >> ~/.bashrc
 echo "alias rm -c='/usr/local/bin/trash -c'" >> ~/.bashrc
+echo "alias rm -l='/usr/local/bin/trash'" >> ~/.bashrc
+source ~/.bashrc
